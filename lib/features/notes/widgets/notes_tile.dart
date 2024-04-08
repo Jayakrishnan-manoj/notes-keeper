5df61b37808791c61902447deed650b087b134aa
Jayakrishnan M <jk.jayakrishnan27@gmail.com>
2024-04-08 18:51:36 +0530

    implement note deletion

--- lib/features/notes/widgets/notes_tile.dart
+++ lib/features/notes/widgets/notes_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_keeper/features/notes/models/note_model.dart';
+import 'package:notes_keeper/shared/DI/locator.dart';
+import 'package:notes_keeper/shared/providers/notes_provider.dart';
 import 'package:notes_keeper/shared/utils/colors.dart';
 
 class NotesTile extends StatelessWidget {
@@ -13,25 +15,25 @@ class NotesTile extends StatelessWidget {
       textColor: kPrimaryColor,
       collapsedIconColor: kTextColor,
       iconColor: kPrimaryColor,
-      title:  Text(
+      title: Text(
         note.content.toString(),
         style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          overflow: TextOverflow.ellipsis
-        ),
+            fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),
       ),
       initiallyExpanded: true,
       children: [
         Text(
           note.content.toString(),
           textAlign: TextAlign.center,
-          style: TextStyle(color: kTextColor),
+          style: const TextStyle(color: kTextColor),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                locator<NotesProvider>().removeNote(note.id.toString());
+              },
               icon: const Icon(
                 Icons.delete,
                 color: Colors.red,
